658526fcb596cfdeb6851b28fb1708d6dba28f23
DT Nesimi <ditinesimi@gmail.com>
2025-05-20 14:34:58 -0400

    basic line created

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,20 +1,37 @@
-// Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(int argc, char** argv) {
+
+	int width = 800;
+	int height = 600;
+
+	ALLEGRO_DISPLAY* display = NULL;
+
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+	display = al_create_display(width, height);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return -1;
+	}
+
+	al_init_primitives_addon();
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_flip_display();
+
+	al_draw_line(0, 0, 100, 100, al_map_rgb(255, 255, 255), 10);
+	al_flip_display();
+
+	al_rest(5.0);
+
+	al_destroy_display(display);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
